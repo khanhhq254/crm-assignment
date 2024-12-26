57d214fc5776459e637c178a0805d68f8eabdd97
quockhanh <quockhanh@thomsonmedical.com>
2024-12-26 09:18:37 +0700

    Update customer dto

--- CRMAPI.Application/Dtos/Customer/CustomerDto.cs
+++ CRMAPI.Application/Dtos/Customer/CustomerDto.cs
@@ -2,7 +2,7 @@ namespace CRMAPI.Application.Dtos.Customer;
 
 public class CustomerDto
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public bool IsLead { get; set; }
--- CRMAPI.Application/Dtos/PricingAgreement/PricingAgreementDto.cs
+++ CRMAPI.Application/Dtos/PricingAgreement/PricingAgreementDto.cs
@@ -2,9 +2,9 @@ namespace CRMAPI.Application.Dtos.PricingAgreement;
 
 public class PricingAgreementDto
 {
-    public Guid Id { get; set; }
-    public Guid CustomerId { get; set; }
-    public Guid ProductId { get; set; }
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int ProductId { get; set; }
     public decimal AgreedPrice { get; set; }
     public DateTime AgreementDate { get; set; }
 }
